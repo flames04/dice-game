4c9e2dbd1519276d51ed3bb9908f6ca266237863
Ea Francisco <emberkasai04@gmail.com>
2021-11-25 10:35:25 -0500

    add random roll number

--- js/script.js
+++ js/script.js
@@ -7,10 +7,12 @@ const getRandomDiceRoll = function(sides=6) {
 }
 
 // Here's a test roll, check the console!
-console.log(getRandomDiceRoll())
+//console.log(getRandomDiceRoll())
 
 // 1) Call getRandomDiceRoll() and store the result as a variable named "diceRoll"
+const diceRoll = getRandomDiceRoll();
 
+console.log(`You rolled ${diceRoll}`);
 // 2) Update user interface (document), showing the diceface (svg image) that matches the roll number
 
 // 3) Use `diceRoll` to update the label "You rolled: #" (replacing # with the roll)
