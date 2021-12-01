794353a570a2a50f8ee01bb4e114ae92bf586608
Ea Francisco <emberkasai04@gmail.com>
2021-12-01 17:44:19 -0500

    update javascript to follow best practices

--- js/script.js
+++ js/script.js
@@ -5,38 +5,54 @@
 const getRandomDiceRoll = function(sides=6) {
   return Math.floor( Math.random() * sides ) + 1
 }
-
 // Here's a test roll, check the console!
 //console.log(getRandomDiceRoll())
 
 // 1) Call getRandomDiceRoll() and store the result as a variable named "diceRoll"
 
-
 // 2) Update user interface (document), showing the diceface (svg image) that matches the roll number
-
 
 // 3) Use `diceRoll` to update the label "You rolled: #" (replacing # with the roll)
 
 // 4) Wrap the dice roll procedure in a function named rollTheDice(), call it from the console to test
+
+// 5) Keep track of the previous roll value, such that if the same number is rolled twice consecutively, the output message will notify the user appropriately
+// 6) Add a second dice to the application interface to mimic the results of the popular casino game, "craps". Replace the result message ("Your rolled: ____") with the appropriate roll outcome based on the table below, reflecting the result of rolling both dice together
+const diceMessage = document.querySelector(`h2`);
+diceMessage.textContent = `Your rolled four!`;
+
+const eleDice = document.querySlector(`.dice`);
+
+eleDice.setAttribute(`src`, `img/dice3.svg`);
+
+const diceRoll = 3;
+
+
+
 let lastRoll = 0;
 
 const rollTheDice = function() {
   //step 1)
   let diceRollOne = getRandomDiceRoll(); //generates random number
-  let diceRollTwo = getRandomDiceRoll (); //second dice
+  //let diceRollTwo = getRandomDiceRoll (); //second dice
 
   let diceRoll = (diceRollOne + diceRollTwo);
+  
+  if (diceRoll === 2){
+    rolledDice = `Snake Eyes`;
+  } else if (diceRoll ===)
+  
 
-  if (diceRoll === lastRoll) {
-    console.log(`You rolled: ${diceRoll} again`);
-  } else {
-    console.log(`Not the same`);
-  }
-  lastRoll = diceRoll;
+
   //step 3)
   const rolledDice = `You rolled: ${diceRoll}`;
   document.querySelector(`h2`).textContent = rolledDice; //shows the random number in content and updates text
 
+  //condition for rolling same number consecutively
+  if (diceRoll === lastRoll) {
+    rolledDice = `You rolled: ${diceRoll} again!`;
+  };
+  
   //step 2)
   document.querySelector(`.one`).src = `../img/dice${diceRollOne}.svg`;
   document.querySelector(`.two`).src = `../img/dice${diceRollTwo}.svg`;
